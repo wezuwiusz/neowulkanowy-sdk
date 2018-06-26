2d7abcfc6aa2e124ef4492b72ab1bb59b92cfc4d
Mikołaj Pich <m.pich@outlook.com>
2018-06-26 17:28:48 +0200

    Add base files

---
+++ src/main/kotlin/io/github/wulkanowy/sdk/ApiSignInterceptor.kt
@@ -0,0 +1,12 @@
+package io.github.wulkanowy.sdk
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ApiSignInterceptor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain?): Response {
+        TODO("not implemented")
+    }
+
+}
