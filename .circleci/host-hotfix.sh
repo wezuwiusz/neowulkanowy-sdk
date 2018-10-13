6bb2e6801aa2cab71090d787cc97bbde84713c46
Mikołaj Pich <m.pich@outlook.com>
2018-10-13 13:34:41 +0200

    Fix UnknownHostException

--- .circleci/host-hotfix.sh
+++ .circleci/host-hotfix.sh
@@ -2,6 +2,7 @@
 
 echo "127.0.0.1 fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 adfs.fakelog.localhost" | sudo tee -a /etc/hosts
+echo "127.0.0.1 adfslight.fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 cufs.fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 uonetplus.fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 uonetplus-opiekun.fakelog.localhost" | sudo tee -a /etc/hosts
