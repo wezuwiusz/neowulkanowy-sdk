d779c35e001a4445611a237f3c90d9258e1e1bb4
Mikołaj Pich <m.pich@outlook.com>
2018-10-16 17:16:02 +0200

    Make sendCredentials() more readable

--- src/main/kotlin/io/github/wulkanowy/api/repository/LoginRepository.kt
+++ src/main/kotlin/io/github/wulkanowy/api/repository/LoginRepository.kt
@@ -36,59 +36,62 @@ class LoginRepository(
                     "Password" to password
             ))
 
-            Api.LoginType.ADFSLight -> api.getADFSLightForm("$schema://cufs.$host/$symbol/").flatMap {
-                api.sendADFSForm("$schema://adfslight.$host/${it.formAction.removePrefix("/")}", mapOf(
-                        "Username" to email,
-                        "Password" to email
-                ))
-            }
+            Api.LoginType.ADFSLight -> api.getADFSLightForm("$schema://cufs.$host/$symbol/")
+                    .flatMap {
+                        api.sendADFSForm("$schema://adfslight.$host/${it.formAction.removePrefix("/")}", mapOf(
+                                "Username" to email,
+                                "Password" to email
+                        ))
+                    }
 
-            Api.LoginType.ADFS -> api.getForm(firstStepReturnUrl).flatMap {
-                api.sendADFSForm("$schema://adfs.$host/${it.formAction.removePrefix("/")}", mapOf(
-                        "__db" to it.db,
-                        "__VIEWSTATE" to it.viewstate,
-                        "__VIEWSTATEGENERATOR" to it.viewstateGenerator,
-                        "__EVENTVALIDATION" to it.eventValidation,
-                        "UsernameTextBox" to email,
-                        "PasswordTextBox" to password,
-                        "SubmitButton.x" to "0",
-                        "SubmitButton.y" to "0"
-                ))
-            }.flatMap {
-                api.sendADFSForm(it.action, mapOf(
-                        "wa" to it.wa,
-                        "wresult" to it.wresult,
-                        "wctx" to it.wctx
-                ))
-            }
+            Api.LoginType.ADFS -> api.getForm(firstStepReturnUrl)
+                    .flatMap {
+                        api.sendADFSForm("$schema://adfs.$host/${it.formAction.removePrefix("/")}", mapOf(
+                                "__db" to it.db,
+                                "__VIEWSTATE" to it.viewstate,
+                                "__VIEWSTATEGENERATOR" to it.viewstateGenerator,
+                                "__EVENTVALIDATION" to it.eventValidation,
+                                "UsernameTextBox" to email,
+                                "PasswordTextBox" to password,
+                                "SubmitButton.x" to "0",
+                                "SubmitButton.y" to "0"
+                        ))
+                    }.flatMap {
+                        api.sendADFSForm(it.action, mapOf(
+                                "wa" to it.wa,
+                                "wresult" to it.wresult,
+                                "wctx" to it.wctx
+                        ))
+                    }
 
-            Api.LoginType.ADFSCards -> api.getForm(firstStepReturnUrl).flatMap {
-                api.sendADFSFormStandardChoice("$schema://adfs.$host/${it.formAction.removePrefix("/")}", mapOf(
-                        "__db" to it.db,
-                        "__VIEWSTATE" to it.viewstate,
-                        "__VIEWSTATEGENERATOR" to it.viewstateGenerator,
-                        "__EVENTVALIDATION" to it.eventValidation,
-                        "PassiveSignInButton.x" to "0",
-                        "PassiveSignInButton.y" to "0"
-                ))
-            }.flatMap {
-                api.sendADFSForm("$schema://adfs.$host/${it.formAction.removePrefix("/")}", mapOf(
-                        "__db" to it.db,
-                        "__VIEWSTATE" to it.viewstate,
-                        "__VIEWSTATEGENERATOR" to it.viewstateGenerator,
-                        "__EVENTVALIDATION" to it.eventValidation,
-                        "SubmitButton.x" to "0",
-                        "SubmitButton.y" to "0",
-                        "UsernameTextBox" to email,
-                        "PasswordTextBox" to password
-                ))
-            }.flatMap {
-                api.sendADFSForm(it.action, mapOf(
-                        "wa" to it.wa,
-                        "wresult" to it.wresult,
-                        "wctx" to it.wctx
-                ))
-            }
+            Api.LoginType.ADFSCards -> api.getForm(firstStepReturnUrl)
+                    .flatMap {
+                        api.sendADFSFormStandardChoice("$schema://adfs.$host/${it.formAction.removePrefix("/")}", mapOf(
+                                "__db" to it.db,
+                                "__VIEWSTATE" to it.viewstate,
+                                "__VIEWSTATEGENERATOR" to it.viewstateGenerator,
+                                "__EVENTVALIDATION" to it.eventValidation,
+                                "PassiveSignInButton.x" to "0",
+                                "PassiveSignInButton.y" to "0"
+                        ))
+                    }.flatMap {
+                        api.sendADFSForm("$schema://adfs.$host/${it.formAction.removePrefix("/")}", mapOf(
+                                "__db" to it.db,
+                                "__VIEWSTATE" to it.viewstate,
+                                "__VIEWSTATEGENERATOR" to it.viewstateGenerator,
+                                "__EVENTVALIDATION" to it.eventValidation,
+                                "SubmitButton.x" to "0",
+                                "SubmitButton.y" to "0",
+                                "UsernameTextBox" to email,
+                                "PasswordTextBox" to password
+                        ))
+                    }.flatMap {
+                        api.sendADFSForm(it.action, mapOf(
+                                "wa" to it.wa,
+                                "wresult" to it.wresult,
+                                "wctx" to it.wctx
+                        ))
+                    }
         }
     }
 
