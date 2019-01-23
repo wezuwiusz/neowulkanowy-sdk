cea28b77c6a1a5cf6c5abdba1007d8d02656976d
Mikołaj Pich <m.pich@outlook.com>
2019-01-23 20:24:09 +0100

    Temporary disable share of login helper's flowable

--- src/main/kotlin/io/github/wulkanowy/api/service/ServiceManager.kt
+++ src/main/kotlin/io/github/wulkanowy/api/service/ServiceManager.kt
@@ -148,7 +148,7 @@ class ServiceManager(
         return LoginRepository(loginType, schema, host, symbol, cookies, getLoginService())
                 .login(email, password)
                 .toFlowable()
-                .share()
+//                .share()
     }
 
     class UrlGenerator(private val schema: String, private val host: String, var symbol: String, var schoolId: String) {
