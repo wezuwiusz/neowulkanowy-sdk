1897a0ed07ff7dfa607b9d2cff4b0e4c36f947af
Mikołaj Pich <m.pich@outlook.com>
2019-02-05 23:32:40 +0100

    Add tests to notes from the new student module

--- src/test/kotlin/io/github/wulkanowy/api/notes/NotesTest.kt
+++ src/test/kotlin/io/github/wulkanowy/api/notes/NotesTest.kt
@@ -6,47 +6,35 @@ import org.junit.Test
 
 class NotesTest : BaseLocalTest() {
 
-    private val filled by lazy {
-        getSnpRepo(NotesTest::class.java, "UwagiOsiagniecia-filled.html").getNotes().blockingGet()
-    }
-
     private val empty by lazy {
         getSnpRepo(NotesTest::class.java, "UwagiOsiagniecia-empty.html").getNotes().blockingGet()
     }
 
+    private val snp by lazy {
+        getSnpRepo(NotesTest::class.java, "UwagiOsiagniecia-filled.html").getNotes().blockingGet()
+    }
+
+    private val student by lazy {
+        getStudentRepo(NotesTest::class.java, "UwagiIOsiagniecia.json").getNotes().blockingGet()
+    }
+
     @Test
-    fun getAllNotesTest() {
-        assertEquals(3, filled.size)
+    fun getNotesList() {
+        assertEquals(3, snp.size)
+        assertEquals(3, student.size)
         assertEquals(0, empty.size)
     }
 
     @Test
-    fun getDateTest() {
-        assertEquals(getDate(2016, 10, 1), filled[0].date)
-        assertEquals(getDate(2017, 6, 6), filled[2].date)
-    }
-
-    @Test
-    fun getTeacherTest() {
-        assertEquals("Kochański Leszek", filled[0].teacher)
-        assertEquals("Jan Kowalski", filled[2].teacher)
-    }
-
-    @Test
-    fun getTeacherSymbol() {
-        assertEquals("KL", filled[0].teacherSymbol)
-        assertEquals("JK", filled[2].teacherSymbol)
-    }
-
-    @Test
-    fun getCategoryTest() {
-        assertEquals("Zachowanie na lekcji", filled[0].category)
-        assertEquals("Zaangażowanie społeczne", filled[2].category)
-    }
-
-    @Test
-    fun getContentTest() {
-        assertEquals("Przeszkadzanie w prowadzeniu lekcji", filled[0].content)
-        assertEquals("Pomoc przy pikniku charytatywnym", filled[2].content)
+    fun getNotes() {
+        listOf(snp[0], student[0]).map {
+            it.run {
+                assertEquals(getDate(2016, 10, 1), date)
+                assertEquals("Kochański Leszek", teacher)
+                assertEquals("KL", teacherSymbol)
+                assertEquals("Zachowanie na lekcji", category)
+                assertEquals("Przeszkadzanie w prowadzeniu lekcji", content)
+            }
+        }
     }
 }
