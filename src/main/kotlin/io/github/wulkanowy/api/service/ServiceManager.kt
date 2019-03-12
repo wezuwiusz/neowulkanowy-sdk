1cfc711a2344b7b98b94aaedc8a17310458fcc10
Mikołaj Pich <m.pich@outlook.com>
2019-03-12 18:10:24 +0100

    Use share() on LoginHelper

--- src/main/kotlin/io/github/wulkanowy/api/service/ServiceManager.kt
+++ src/main/kotlin/io/github/wulkanowy/api/service/ServiceManager.kt
@@ -154,7 +154,7 @@ class ServiceManager(
         return LoginHelper(loginType, schema, host, symbol, cookies, getLoginService())
                 .login(email, password)
                 .toFlowable()
-//                .share()
+                .share()
     }
 
     class UrlGenerator(private val schema: String, private val host: String, var symbol: String, var schoolId: String) {
