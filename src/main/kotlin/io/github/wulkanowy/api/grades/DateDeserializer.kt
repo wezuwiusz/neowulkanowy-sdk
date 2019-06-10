467637c55d4305943240c58e08b6d3d0ebb0e7aa
Mikołaj Pich <m.pich@outlook.com>
2019-06-10 18:05:22 +0200

    Optimize DateDeserializer

--- src/main/kotlin/io/github/wulkanowy/api/grades/DateDeserializer.kt
+++ src/main/kotlin/io/github/wulkanowy/api/grades/DateDeserializer.kt
@@ -6,25 +6,18 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonParseException
 import io.github.wulkanowy.api.toDate
 import java.lang.reflect.Type
-import java.text.ParseException
 import java.util.Date
 
 class DateDeserializer<T : Date>(private val mClazz: Class<T>) : JsonDeserializer<T> {
 
     override fun deserialize(element: JsonElement, arg1: Type, context: JsonDeserializationContext): T {
         val dateString = element.asString
-        try {
-            return mClazz.newInstance().apply {
+        return try {
+            mClazz.newInstance().apply {
                 time = dateString.toDate(GradeDate.FORMAT).time
             }
-        } catch (e: InstantiationException) {
-            throw JsonParseException(e.message, e)
-        } catch (e: IllegalAccessException) {
-            throw JsonParseException(e.message, e)
-        } catch (e: ParseException) {
-            throw JsonParseException(e.message, e)
-        } catch (e: ArrayIndexOutOfBoundsException) {
-            throw IllegalArgumentException("Invalid date format: $dateString", e)
+        } catch (e: Throwable) {
+            throw JsonParseException("Error while parse date format: $dateString", e)
         }
     }
 }
