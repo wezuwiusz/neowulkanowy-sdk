1d0a39dc3a9e98d6a84b1388e6781c5631b99ad0
Mikołaj Pich <m.pich@outlook.com>
2019-10-01 03:01:12 +0200

    Fix exception transformer to transform bad creds exception

--- src/main/kotlin/io/github/wulkanowy/sdk/ScrapperExceptionTransformer.kt
+++ src/main/kotlin/io/github/wulkanowy/sdk/ScrapperExceptionTransformer.kt
@@ -1,5 +1,6 @@
 package io.github.wulkanowy.sdk
 
+import io.github.wulkanowy.api.ApiException as ScrapperApiException
 import io.github.wulkanowy.api.login.BadCredentialsException as ScrapperBadCredentialsException
 import io.github.wulkanowy.sdk.exception.*
 import io.github.wulkanowy.api.interceptor.ServiceUnavailableException as ScrapperServiceUnavailableException
@@ -14,14 +15,15 @@ class ScrapperExceptionTransformer<T : Any?> : SingleTransformer<T, T> {
 
     override fun apply(upstream: Single<T>): SingleSource<T> {
         return upstream.onErrorResumeNext {
-            if (it is ScrapperVulcanException) Single.error(when (it) {
+            Single.error(when (it) {
                 is ScrapperFeatureDisabledException -> FeatureDisabledException(it.message ?: "")
                 is ScrapperNotLoggedInException -> NotLoggedInException(it.message ?: "")
                 is ScrapperServiceUnavailableException -> ServiceUnavailableException(it.message ?: "")
                 is ScrapperBadCredentialsException -> BadCredentialsException(it.message ?: "")
-                else -> VulcanException(it.message ?: "")
+                is ScrapperVulcanException -> VulcanException(it.message ?: "")
+                is ScrapperApiException -> ApiException(it.message ?: "")
+                else -> it
             })
-            else Single.error(it)
         }
     }
 }
