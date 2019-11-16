433cd83357a11cbfb3538afaae9f8b2b55a73853
Mikołaj Pich <m.pich@outlook.com>
2019-11-17 00:54:18 +0100

    Filter empty teachers

--- sdk/src/main/kotlin/io/github/wulkanowy/sdk/mapper/TeacherMapper.kt
+++ sdk/src/main/kotlin/io/github/wulkanowy/sdk/mapper/TeacherMapper.kt
@@ -6,11 +6,12 @@ import io.github.wulkanowy.sdk.scrapper.school.Teacher as ScrapperTeacher
 import io.github.wulkanowy.sdk.mobile.school.Teacher as ApiTeacher
 
 fun List<ApiTeacher>.mapTeachers(dictionaries: Dictionaries): List<Teacher> {
-    return map { teacher ->
+    return mapNotNull { teacher ->
         val item = dictionaries.employees.singleOrNull { it.id == teacher.employeeId }
+        if (item?.name == null) return@mapNotNull null
         Teacher(
-            name = "${item?.name} ${item?.surname}",
-            short = "${item?.code}",
+            name = "${item.name} ${item.surname}",
+            short = item.code,
             subject = dictionaries.subjects.singleOrNull { it.id == teacher.subjectId }?.name ?: teacher.role
         )
     }
