ab5c08f2d43f9398af019a9126a631483ce12baa
Mikołaj Pich <m.pich@outlook.com>
2019-12-04 17:17:37 +0100

    Change symbol to schoolSymbol distinct in hybrid device register

--- sdk/src/main/kotlin/io/github/wulkanowy/sdk/Sdk.kt
+++ sdk/src/main/kotlin/io/github/wulkanowy/sdk/Sdk.kt
@@ -187,7 +187,7 @@ class Sdk {
     fun getStudentsHybrid(email: String, password: String, scrapperBaseUrl: String, startSymbol: String = "Default", apiKey: String = ""): Single<List<Student>> {
         return getStudentsFromScrapper(email, password, scrapperBaseUrl, startSymbol)
             .compose(ScrapperExceptionTransformer())
-            .map { students -> students.distinctBy { it.symbol } }
+            .map { students -> students.distinctBy { it.schoolSymbol } }
             .flatMapObservable { Observable.fromIterable(it) }
             .flatMapSingle { scrapperStudent ->
                 scrapper.let {
