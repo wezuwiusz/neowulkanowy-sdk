1f7fab1740c7d0eadf18eab88b09a848575549c3
Mikołaj Pich <m.pich@outlook.com>
2020-03-02 20:52:03 +0100

    Distinct students by symbol in hybrid login

--- sdk/src/main/kotlin/io/github/wulkanowy/sdk/Sdk.kt
+++ sdk/src/main/kotlin/io/github/wulkanowy/sdk/Sdk.kt
@@ -193,7 +193,7 @@ class Sdk {
     fun getStudentsHybrid(email: String, password: String, scrapperBaseUrl: String, startSymbol: String = "Default", apiKey: String = ""): Single<List<Student>> {
         return getStudentsFromScrapper(email, password, scrapperBaseUrl, startSymbol)
             .compose(ScrapperExceptionTransformer())
-            .map { students -> students.distinctBy { it.schoolSymbol } }
+            .map { students -> students.distinctBy { it.symbol } }
             .flatMapObservable { Observable.fromIterable(it) }
             .flatMapSingle { scrapperStudent ->
                 scrapper.let {
