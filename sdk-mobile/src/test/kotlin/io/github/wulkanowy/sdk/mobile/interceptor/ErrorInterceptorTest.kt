cf36fcf956d71e1e225d8a817461af9858b3f706
Mikołaj Pich <m.pich@outlook.com>
2020-04-10 19:07:38 +0200

    Fix tests

--- sdk-mobile/src/test/kotlin/io/github/wulkanowy/sdk/mobile/interceptor/ErrorInterceptorTest.kt
+++ sdk-mobile/src/test/kotlin/io/github/wulkanowy/sdk/mobile/interceptor/ErrorInterceptorTest.kt
@@ -7,19 +7,20 @@ import io.github.wulkanowy.sdk.mobile.repository.RegisterRepository
 import io.reactivex.observers.TestObserver
 import org.junit.Test
 import retrofit2.create
+import java.io.IOException
 
 class ErrorInterceptorTest : BaseLocalTest() {
 
     @Test
-    fun invalidSymbol() {
-        server.enqueueAndStart("invalid-symbol.txt")
+    fun unknownError() {
+        server.enqueueAndStart("bad-request.txt")
 
         val repo = RegisterRepository(getRetrofitBuilder().baseUrl("http://localhost:3030/").build().create())
         val students = repo.getStudents()
         val studentsObserver = TestObserver<List<Student>>()
         students.subscribe(studentsObserver)
         studentsObserver.assertNotComplete()
-        studentsObserver.assertError(InvalidSymbolException::class.java)
+        studentsObserver.assertError(IOException::class.java)
     }
 
     @Test
