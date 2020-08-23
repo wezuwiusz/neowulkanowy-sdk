2345dea5a59de9cb1f9fdff96aaa0c3b3b721ac3
Mikołaj Pich <m.pich@outlook.com>
2020-08-23 19:54:05 +0200

    Fix tests

--- .circleci/host-hotfix.sh
+++ .circleci/host-hotfix.sh
@@ -5,5 +5,5 @@ echo "127.0.0.1 adfs.fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 adfslight.fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 cufs.fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 uonetplus.fakelog.localhost" | sudo tee -a /etc/hosts
-echo "127.0.0.1 uonetplus-opiekun.fakelog.localhost" | sudo tee -a /etc/hosts
+echo "127.0.0.1 uonetplus-uzytkownik.fakelog.localhost" | sudo tee -a /etc/hosts
 echo "127.0.0.1 uonetplus-uczen.fakelog.localhost" | sudo tee -a /etc/hosts
