6b9fd68ba810ac8719fb4f77228dbf21aec84e43
Mikołaj Pich <m.pich@outlook.com>
2020-09-28 23:06:37 +0200

    Fix UnregisterDeviceRequest moshi adapter

--- sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/mobile/UnregisterDeviceRequest.kt
+++ sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/mobile/UnregisterDeviceRequest.kt
@@ -1,5 +1,11 @@
 package io.github.wulkanowy.sdk.scrapper.mobile
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class UnregisterDeviceRequest(
+
+    @Json(name = "id")
     val id: Int
 )
