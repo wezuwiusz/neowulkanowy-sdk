7efab423b544d8488985286009de24f8bd0257f8
Mikołaj Pich <m.pich@outlook.com>
2021-12-29 13:35:37 +0100

    Block sdk tz at Europe/Warsaw

--- sdk/src/main/kotlin/io/github/wulkanowy/sdk/Utils.kt
+++ sdk/src/main/kotlin/io/github/wulkanowy/sdk/Utils.kt
@@ -3,25 +3,27 @@ package io.github.wulkanowy.sdk
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.ZoneId.systemDefault
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter.ofPattern
 import java.util.Date
+
+private val registerTimeZone = ZoneId.of("Europe/Warsaw")
 
 fun String.toLocalDateTime(format: String): LocalDateTime = LocalDateTime.parse(this, ofPattern(format))
 
 fun Long.toLocalDate(): LocalDate = Instant
     .ofEpochMilli(this * 1000L)
-    .atZone(systemDefault())
+    .atZone(registerTimeZone)
     .toLocalDate()
 
 fun Long.toLocalDateTime(): LocalDateTime = Instant
     .ofEpochMilli(this * 1000L)
-    .atZone(systemDefault())
+    .atZone(registerTimeZone)
     .toLocalDateTime()
 
 fun Date.toLocalDateTime(): LocalDateTime = Instant
     .ofEpochMilli(time)
-    .atZone(systemDefault())
+    .atZone(registerTimeZone)
     .toLocalDateTime()
 
 fun String.normalizeRecipient() = substringBeforeLast("-").substringBefore(" [").substringBeforeLast(" (").trim()
