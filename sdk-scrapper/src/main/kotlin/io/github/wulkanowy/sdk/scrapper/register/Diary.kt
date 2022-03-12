fba873461e9c70d8b557b9497fc3906d77449c17
Mikołaj Pich <m.pich@outlook.com>
2022-03-12 12:17:20 +0100

    Add more fields to Diary model

--- sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/register/Diary.kt
+++ sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/register/Diary.kt
@@ -22,6 +22,9 @@ data class Diary(
     @Json(name = "UczenNazwisko")
     val studentSurname: String,
 
+    @Json(name = "UczenPseudonim")
+    val studentNick: String?,
+
     @Json(name = "IsDziennik")
     val isDiary: Boolean,
 
@@ -30,6 +33,9 @@ data class Diary(
 
     @Json(name = "IdPrzedszkoleDziennik")
     val kindergartenDiaryId: Int,
+
+    @Json(name = "IdWychowankowieDziennik")
+    val fosterDiaryId: Int?,
 
     @Json(name = "Poziom")
     val level: Int,
@@ -46,14 +52,74 @@ data class Diary(
     @Json(name = "Okresy")
     val semesters: List<Semester>? = emptyList(),
 
+    @Json(name = "UczenOddzialOkresy")
+    val classSemesters: List<Any>? = emptyList(),
+
     @Json(name = "DziennikDataOd")
     val start: Date,
 
     @Json(name = "DziennikDataDo")
     val end: Date,
 
+    @Json(name = "IdJednostkaSkladowa")
+    val componentUnitId: Int?,
+
+    @Json(name = "IdSioTyp")
+    val sioTypeId: Int?,
+
+    @Json(name = "IsDorosli")
+    val isAdults: Boolean?,
+
+    @Json(name = "IsPolicealna")
+    val isPostSecondary: Boolean?,
+
+    @Json(name = "Is13")
+    val is13: Boolean?,
+
+    @Json(name = "IsArtystyczna")
+    val isArtistic: Boolean?,
+
+    @Json(name = "IsArtystyczna13")
+    val isArtistic13: Boolean?,
+
+    @Json(name = "IsSpecjalny")
+    val isSpecial: Boolean?,
+
+    @Json(name = "IsPrzedszkola")
+    val isKindergarten: Boolean?,
+
+    @Json(name = "IsWychowankowie")
+    val isFoster: Boolean?,
+
+    @Json(name = "IsArchiwalny")
+    val isArchived: Boolean?,
+
+    @Json(name = "IsOplaty")
+    val isCharges: Boolean?,
+
+    @Json(name = "IsPlatnosci")
+    val isPayments: Boolean?,
+
+    @Json(name = "IsPayButtonOn")
+    val isPayButtonOn: Boolean?,
+
+    @Json(name = "CanMergeAccounts")
+    val canMergeAccounts: Boolean?,
+
     @Json(name = "UczenPelnaNazwa")
-    val fullName: String
+    val fullName: String,
+
+    @Json(name = "O365PassType")
+    val o365PassType: Int?,
+
+    @Json(name = "IsAdult")
+    val isAdult: Boolean?,
+
+    @Json(name = "IsAuthorized")
+    val isAuthorized: Boolean?,
+
+    @Json(name = "Obywatelstwo")
+    val citizenship: Int?,
 ) {
     @JsonClass(generateAdapter = true)
     data class Semester(
