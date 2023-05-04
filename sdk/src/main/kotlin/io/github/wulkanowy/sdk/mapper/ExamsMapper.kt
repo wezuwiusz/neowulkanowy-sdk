f42afd1602916190911923dbe6779456c0232a3e
Mikołaj Pich <m.pich@outlook.com>
2023-05-04 23:49:12 +0200

    Fix exam dates

--- sdk/src/main/kotlin/io/github/wulkanowy/sdk/mapper/ExamsMapper.kt
+++ sdk/src/main/kotlin/io/github/wulkanowy/sdk/mapper/ExamsMapper.kt
@@ -21,8 +21,8 @@ internal fun List<ScrapperExam>.mapExams() = map {
 @JvmName("mapHebeExams")
 internal fun List<HebeExam>.mapExams() = map {
     Exam(
-        date = it.dateCreated.date,
-        entryDate = it.deadline.date,
+        date = it.deadline.date,
+        entryDate = it.dateCreated.date,
         description = it.content,
         subject = it.subject.name,
         teacher = it.creator.displayName,
