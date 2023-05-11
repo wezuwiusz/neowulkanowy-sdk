c1573f04c5438f7552240f59968d4c1928bbd7b0
Mikołaj Pich <m.pich@outlook.com>
2023-05-11 10:57:35 +0200

    Add AuthorizationRequiredException

---
+++ sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/exception/AuthorizationRequiredException.kt
@@ -0,0 +1,3 @@
+package io.github.wulkanowy.sdk.scrapper.exception
+
+class AuthorizationRequiredException(message: String) : VulcanException(message)
--- sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/interceptor/ErrorHandlerTransformer.kt
+++ sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/interceptor/ErrorHandlerTransformer.kt
@@ -1,17 +1,17 @@
 package io.github.wulkanowy.sdk.scrapper.interceptor
 
 import io.github.wulkanowy.sdk.scrapper.ApiResponse
-import io.github.wulkanowy.sdk.scrapper.exception.ScrapperException
+import io.github.wulkanowy.sdk.scrapper.exception.AuthorizationRequiredException
 import io.github.wulkanowy.sdk.scrapper.exception.FeatureDisabledException
 import io.github.wulkanowy.sdk.scrapper.exception.InvalidPathException
+import io.github.wulkanowy.sdk.scrapper.exception.ScrapperException
 import io.github.wulkanowy.sdk.scrapper.exception.VulcanException
-import io.github.wulkanowy.sdk.scrapper.login.AccountPermissionException
 
 internal fun <T> ApiResponse<T>.handleErrors(): ApiResponse<T> {
     return if (!success && feedback != null) throw feedback.run {
         when {
             message.contains("niespójność danych") -> ScrapperException(message)
-            message.contains("Brak uprawnień") -> AccountPermissionException(message)
+            message.contains("Brak uprawnień") -> AuthorizationRequiredException(message)
             message.contains("wyłączony") -> FeatureDisabledException(message)
             message.contains("DB_ERROR") -> VulcanException(message)
             message.contains("błąd") -> VulcanException(message)
