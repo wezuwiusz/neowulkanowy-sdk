e73761051ac3559c10040d894059b6e461892479
Mikołaj Pich <m.pich@outlook.com>
2024-03-19 23:55:57 +0100

    Add error body to http errors if content body is json

--- sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/interceptor/HttpErrorInterceptor.kt
+++ sdk-scrapper/src/main/kotlin/io/github/wulkanowy/sdk/scrapper/interceptor/HttpErrorInterceptor.kt
@@ -14,19 +14,23 @@ internal class HttpErrorInterceptor : Interceptor {
 
         if (response.isSuccessful || response.isRedirect) return response
 
+        val message = if (response.body?.contentType()?.subtype == "json") {
+            response.body?.string().orEmpty()
+        } else response.message
+
         return when (response.code) {
-            429 -> throw NotLoggedInException(response.body?.string() ?: response.message)
-            404 -> throw ScrapperException(response.code.toString() + ": " + response.message, response.code)
-            in 400..402 -> throw VulcanClientError(response.code.toString() + ": " + response.body?.string(), response.code)
+            429 -> throw NotLoggedInException(message)
+            404 -> throw ScrapperException(response.code.toString() + ": " + message, response.code)
+            in 400..402 -> throw VulcanClientError(response.code.toString() + ": " + message, response.code)
             403 -> {
                 if (response.body?.contentType()?.subtype == "json") {
-                    throw VulcanClientError(response.code.toString() + ": " + response.body?.string(), response.code)
+                    throw VulcanClientError(response.code.toString() + ": " + message, response.code)
                 }
                 response
             }
 
-            in 405..499 -> throw VulcanClientError(response.code.toString() + ": " + response.body?.string(), response.code)
-            in 500..599 -> throw ServiceUnavailableException(response.code.toString() + ": " + response.message)
+            in 405..499 -> throw VulcanClientError(response.code.toString() + ": " + message, response.code)
+            in 500..599 -> throw ServiceUnavailableException(response.code.toString() + ": " + message)
             else -> response
         }
     }
