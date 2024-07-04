8edbaa68e9d275f541d0e754b03671521095bd45
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-07-04 13:55:31 +0200

    Hebe: Create Teacher model

---
+++ sdk-hebe/src/main/kotlin/io/github/wulkanowy/sdk/hebe/models/Teacher.kt
@@ -0,0 +1,20 @@
+package io.github.wulkanowy.sdk.hebe.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Teacher(
+    @SerialName("Id")
+    val id: Int,
+    @SerialName("Name")
+    val name: String,
+    @SerialName("Surname")
+    val surname: String,
+    @SerialName("DisplayName")
+    val displayName: String,
+    @SerialName("Position")
+    val position: Int,
+    @SerialName("Description")
+    val description: String,
+)
