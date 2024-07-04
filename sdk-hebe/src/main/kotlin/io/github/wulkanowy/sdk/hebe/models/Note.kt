4af8cca2c870f29ff335bb7adfc8e09efe81f0ae
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-07-04 14:11:27 +0200

    Create Note model

---
+++ sdk-hebe/src/main/kotlin/io/github/wulkanowy/sdk/hebe/models/Note.kt
@@ -0,0 +1,72 @@
+package io.github.wulkanowy.sdk.hebe.models
+
+import io.github.wulkanowy.sdk.hebe.CustomDateAdapter
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.time.LocalDate
+
+@Serializable
+data class Note(
+    @SerialName("Id")
+    val id: Int,
+    @SerialName("Key")
+    val key: String,
+    @SerialName("IdPupil")
+    val idPupil: Int,
+    @SerialName("Positive")
+    val positive: Boolean,
+    @SerialName("Content")
+    val content: String,
+    @SerialName("Points")
+    val points: Int?,
+) {
+    @Serializable
+    data class DateValid(
+        @SerialName("Date")
+        @Serializable(with = CustomDateAdapter::class)
+        val date: LocalDate,
+        @SerialName("DateDisplay")
+        val dateDisplay: String,
+        @SerialName("Time")
+        val time: String,
+        @SerialName("Timestamp")
+        val timestamp: Long,
+    )
+
+    @Serializable
+    data class DateModify(
+        @SerialName("Date")
+        @Serializable(with = CustomDateAdapter::class)
+        val date: LocalDate,
+        @SerialName("DateDisplay")
+        val dateDisplay: String,
+        @SerialName("Time")
+        val time: String,
+        @SerialName("Timestamp")
+        val timestamp: Long,
+    )
+
+    @Serializable
+    data class Creator(
+        @SerialName("Id")
+        val id: Int,
+        @SerialName("Surname")
+        val surname: String,
+        @SerialName("Name")
+        val name: String,
+        @SerialName("DisplayName")
+        val displayName: String,
+    )
+
+    @Serializable
+    data class Category(
+        @SerialName("Id")
+        val id: Int,
+        @SerialName("Name")
+        val name: String,
+        @SerialName("Type")
+        val type: String?,
+        @SerialName("DefaultPoints")
+        val defaultPoints: Int?,
+    )
+}
