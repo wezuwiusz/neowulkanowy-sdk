7bf3632faaa19da6a36e50da1f6a8b9f910d4688
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-07-04 15:42:47 +0200

    Add Lesson model

---
+++ sdk-hebe/src/main/kotlin/io/github/wulkanowy/sdk/hebe/models/Lesson.kt
@@ -0,0 +1,107 @@
+package io.github.wulkanowy.sdk.hebe.models
+
+import io.github.wulkanowy.sdk.hebe.CustomDateAdapter
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.time.LocalDate
+
+@Serializable
+data class Lesson(
+    @SerialName("Id")
+    val id: Int,
+    @SerialName("MergeChangeId")
+    val mergeChangeId: Int?,
+    @SerialName("Date")
+    val date: Date,
+    @SerialName("Room")
+    val room: Room,
+    @SerialName("TimeSlot")
+    val timeSlot: TimeSlot,
+    @SerialName("Subject")
+    val subject: Subject,
+    @SerialName("TeacherPrimary")
+    val teacherPrimary: Teacher,
+    @SerialName("TeacherSecondary")
+    val teacherSecondary: Teacher?,
+    @SerialName("TeacherSecondary2")
+    val teacherTertiary: Teacher?,
+    @SerialName("Clazz")
+    val `class`: Class,
+    @SerialName("PupilAlias")
+    val pupilAlias: String?,
+    @SerialName("Visible")
+    val visible: Boolean,
+) {
+    @Serializable
+    data class Date(
+        @SerialName("Date")
+        @Serializable(with = CustomDateAdapter::class)
+        val date: LocalDate,
+        @SerialName("DateDisplay")
+        val dateDisplay: String,
+        @SerialName("Time")
+        val time: String,
+        @SerialName("Timestamp")
+        val timestamp: Long,
+    )
+
+    @Serializable
+    data class Room(
+        @SerialName("Id")
+        val id: Int,
+        @SerialName("Code")
+        val code: String,
+    )
+
+    @Serializable
+    data class TimeSlot(
+        @SerialName("Id")
+        val id: Int,
+        @SerialName("Start")
+        val start: String,
+        @SerialName("End")
+        val end: String,
+        @SerialName("Display")
+        val display: String,
+        @SerialName("Position")
+        val position: Int,
+    )
+
+    @Serializable
+    data class Subject(
+        @SerialName("Id")
+        val id: Int,
+        @SerialName("Key")
+        val key: String,
+        @SerialName("Name")
+        val name: String,
+        @SerialName("Kod")
+        val kod: String,
+        @SerialName("Position")
+        val position: Int,
+    )
+
+    @Serializable
+    data class Teacher(
+        @SerialName("Id")
+        val id: Int,
+        @SerialName("Name")
+        val name: String,
+        @SerialName("Surname")
+        val surname: String,
+        @SerialName("DisplayName")
+        val displayName: String,
+    )
+
+    @Serializable
+    data class Class(
+        @SerialName("Id")
+        val id: Int,
+        @SerialName("Key")
+        val key: String,
+        @SerialName("DisplayName")
+        val displayName: String,
+        @SerialName("Symbol")
+        val symbol: String,
+    )
+}
