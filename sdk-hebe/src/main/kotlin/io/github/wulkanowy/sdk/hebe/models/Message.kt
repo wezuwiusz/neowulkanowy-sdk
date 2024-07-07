7822171e1bcec5ff17df6508ebd4b78a07acc19b
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-07-07 17:18:23 +0200

    Create Message model

---
+++ sdk-hebe/src/main/kotlin/io/github/wulkanowy/sdk/hebe/models/Message.kt
@@ -0,0 +1,86 @@
+package io.github.wulkanowy.sdk.hebe.models
+
+import io.github.wulkanowy.sdk.hebe.CustomDateAdapter
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.time.LocalDate
+
+@Serializable
+data class Message(
+    @SerialName("Id")
+    val id: String,
+    @SerialName("GlobalKey")
+    val globalKey: String,
+    @SerialName("ThreadKey")
+    val threadKey: String,
+    @SerialName("Subject")
+    val subject: String,
+    @SerialName("Content")
+    val content: String,
+    @SerialName("DateSent")
+    val dateSent: DateSent,
+    @SerialName("DateRead")
+    val dateRead: DateRead,
+    @SerialName("Status")
+    val status: Int,
+    @SerialName("Sender")
+    val sender: Sender,
+    @SerialName("Receiver")
+    val receiver: Array<Receiver>,
+    @SerialName("Attachments")
+    val attachments: Array<Attachment>,
+    @SerialName("Importance")
+    val importance: Int,
+    @SerialName("Withdrawn")
+    val withdrawn: Boolean,
+) {
+    @Serializable
+    data class DateSent(
+        @SerialName("Date")
+        @Serializable(with = CustomDateAdapter::class)
+        val date: LocalDate,
+        @SerialName("DateDisplay")
+        val dateDisplay: String,
+        @SerialName("Time")
+        val time: String,
+        @SerialName("Timestamp")
+        val timestamp: Long,
+    )
+
+    @Serializable
+    data class DateRead(
+        @SerialName("Date")
+        @Serializable(with = CustomDateAdapter::class)
+        val date: LocalDate,
+        @SerialName("DateDisplay")
+        val dateDisplay: String,
+        @SerialName("Time")
+        val time: String,
+        @SerialName("Timestamp")
+        val timestamp: Long,
+    )
+
+    @Serializable
+    data class Sender(
+        @SerialName("GlobalKey")
+        val globalKey: String,
+        @SerialName("Name")
+        val name: String,
+    )
+
+    @Serializable
+    data class Receiver(
+        @SerialName("GlobalKey")
+        val globalKey: String,
+        @SerialName("Name")
+        val name: String,
+    )
+
+    @Serializable
+    data class Attachment(
+        @SerialName("Name")
+        val name: String,
+        @SerialName("Link")
+        val link: String,
+    )
+}
